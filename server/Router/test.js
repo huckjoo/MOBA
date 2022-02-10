16bfb1fa3176f4e03609c6d6f537415b7796e7ef
bewisesh91 <bewise.seunghyun@gmail.com>
2022-02-10 17:06:08 +0900

    first commit : project launch

--- server/Router/test.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send({ test: "Console Print : Team MOBA" });
-});
-
-module.exports = router;
