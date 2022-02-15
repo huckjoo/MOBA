9885ad86ce64eb6c45d4e342136e77d5440fc16a
huckjoo <huckjoo96@naver.com>
2022-02-15 14:36:15 +0900

    design: Redesign all pages

--- client/src/components/views/CreateRoomPage/CreateRoom.jsx
+++ client/src/components/views/CreateRoomPage/CreateRoom.jsx
@@ -1,57 +1,69 @@
-import React from "react";
-import { v1 as uuid } from "uuid";
-import styles from "./CreateRoom.module.css";
-import Auth from "../../../hoc/auth";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { v1 as uuid } from 'uuid';
+import styles from './CreateRoom.module.css';
+import Auth from '../../../hoc/auth';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Header from '../../header/Header';
 
 const CreateRoom = (props) => {
   function create() {
     const id = uuid();
     const shopWidth = window.screen.width * 0.85;
     const userWidth = window.screen.width * 0.15;
-    
+
     window.open(
-      "./chooseshop",
-      "zz1",
+      './chooseshop',
+      'shops',
       `width=${shopWidth}, left=${userWidth},top=0,height=10000, scrollbars=yes, resizable, status=yes, menubar=yes, titlebar=yes`
-      // "target"
-      // option_1
     );
 
     window.open(
       `/room/${id}`,
-      `zz`,
-      `width=${userWidth}, top=0, left=-10000, height=600, scrollbars=yes, resizable=no`,
-      "target"
-      // option_2
+      `videochat`,
+      `width=${userWidth}, top=0, left=-10000, height=10000, scrollbars=yes, resizable=no`,
+      'target'
     );
     // props.history.push(`/room/${id}`);
   }
   const navigate = useNavigate();
 
   const logout = () => {
-    axios.get("/api/users/logout").then((response) => {
+    axios.get('/api/users/logout').then((response) => {
       if (response.data.success) {
-        navigate("/login");
+        navigate('/login');
       } else {
-        alert("로그아웃에 실패하였습니다.");
+        alert('로그아웃에 실패하였습니다.');
       }
       console.log(response.data);
     });
   };
 
   return (
-    <div className={styles.background}>
-      <div className={styles.title}>
-        <span>모바</span>
-        <p>함께 쇼핑하는 즐거움</p>
+    <>
+      <Header />
+      <div className={styles.container}>
+        <div className={styles.selectBtns}>
+          <button className={styles.buttons}>친구관리</button>
+          <button className={styles.buttons}>장바구니</button>
+          <button
+            id={styles.shoppingStart}
+            className={styles.buttons}
+            onClick={create}
+          >
+            쇼핑시작
+          </button>
+          <button
+            className={styles.buttons}
+            id={styles.logoutBtn}
+            onClick={logout}
+          >
+            {' '}
+            로그아웃{' '}
+          </button>
+        </div>
       </div>
-      <button className={styles.startBtn} onClick={create}>
-        친구와 함께 쇼핑하기
-      </button>
-      <button onClick={logout}> 로그아웃 </button>
-    </div>
+    </>
   );
 };
 
