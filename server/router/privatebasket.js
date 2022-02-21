b7c1a3c16959edec6b8f6b4efdf57579c3a990d4
bewisesh91 <bewise.seunghyun@gmail.com>
2022-02-21 18:18:20 +0900

    feat : Implement extension parsing

--- server/router/privatebasket.js
+++ server/router/privatebasket.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const basketRouter = express.Router();
 const User = require('../models/User');
+const axios = require('axios')
+const cheerio = require('cheerio')
 
 // TODO : 함께 쇼핑 종료시 유저의 위시리스트에 공유위시리스트 품목 넣기 + 공유 위시리스트 삭제
 
@@ -119,8 +121,156 @@ basketRouter.post('/basket', async (req, res) => {
   const basket_user = await User.findOne({
     token: req.body.token,
   });
-  console.log(basket_user.products);
+  // console.log(basket_user.products);
   res.send(basket_user.products);
 });
+
+
+basketRouter.post('/basketParsing', async (req, res) => {
+  url = req.body.shopUrl
+  const parsing_result = await parse_product(url)
+  res
+    .send(parsing_result);
+});
+
+// w-concept
+function w_concept(html, url) {
+  let shop_name, shop_url, img_url, product_name, price, sale_price;
+  const $ = cheerio.load(html); // html load
+
+  product_name = $("meta[property='og:description']").attr('content');
+  price = $("meta[property='eg:originalPrice']").attr('content');
+  sale_price = $("meta[property='eg:salePrice']").attr('content');
+  shop_name = $("meta[property='og:site_name']").attr('content');
+  shop_url = url;
+  img_url = $("meta[property='og:image']").attr('content');
+
+  const new_product = {
+    product_name: product_name,
+    price: price,
+    sale_price: sale_price,
+    shop_name: shop_name,
+    shop_url: shop_url,
+    img: img_url,
+  };
+
+  return new_product;
+}
+
+// 무신사
+function musinsa(html, url) {
+  let shop_name, shop_url, img_url, product_name, price, sale_price;
+  const $ = cheerio.load(html); // html load
+
+  product_name = $(
+    '#page_product_detail > div.right_area.page_detail_product > div.right_contents.section_product_summary > span > em'
+  ).text();
+  price = $('#goods_price').text().trim();
+  console.log(price);
+  // price parsing - e.g. 110,000원 -> 110000
+  price = Number(
+    price
+      .slice(0, -1)
+      .split(',')
+      .reduce((a, b) => a + b)
+  );
+
+  sale_price = $(
+    '#sPrice > ul > li > span.txt_price_member.m_list_price'
+  ).text();
+  console.log(sale_price);
+  sale_price = Number(
+    sale_price
+      .slice(0, -1)
+      .split(',')
+      .reduce((a, b) => a + b)
+  );
+  shop_name = 'Musinsa';
+  shop_url = url;
+  img_url = $("meta[property='og:image']").attr('content');
+
+  const new_product = {
+    product_name: product_name,
+    price: Number(price),
+    sale_price: sale_price,
+    shop_name: shop_name,
+    shop_url: shop_url,
+    img: img_url,
+  };
+  return new_product;
+}
+
+// 브랜디
+function brandi(html, url) {
+  let shop_name, img_url, product_name, price, sale_price, shop_url;
+  const $ = cheerio.load(html); // html load
+
+  shop_name = '브랜디';
+  shop_url = url;
+  img_url = $("meta[property='og:image']").attr('content');
+  product_name = $(
+    '#container > div > div.wrap-products-info > div.wrap-detail_info > div.detail_basic-info > div.detail_title_area > h1'
+  ).text();
+  price = $(
+    '#container > div > div.wrap-products-info > div.wrap-detail_info > div.detail_basic-info > div.detail-price-wrapper.hideFinalPriceSection > div > div > span > span'
+  ).text();
+  price = Number(price.split(',').reduce((a, b) => a + b));
+  sale_price = $(
+    '#container > div > div.wrap-products-info > div.wrap-detail_info > div.detail_basic-info > div.detail-price-wrapper.hideFinalPriceSection > div > div > em > span'
+  ).text();
+  sale_price = Number(sale_price.split(',').reduce((a, b) => a + b));
+
+  const new_product = {
+    product_name: product_name,
+    price: price,
+    sale_price: sale_price,
+    shop_name: shop_name,
+    shop_url: shop_url,
+    img: img_url,
+  };
+  console.log(new_product);
+
+  return new_product;
+}
+
+async function parse_product(url) {
+  let new_product;
+  console.log(url);
+  const split_url = url.split('/');
+  const cur_shop = split_url[2];
+  // 서비스 가능한 사이트만 req 요청 보내기
+  console.log(`is cur_shop? ${cur_shop}`)
+  if (
+    ['www.wconcept.co.kr', 'store.musinsa.com', 'www.brandi.co.kr'].includes(
+      cur_shop
+    )
+  ) {
+    await axios
+      .get(url)
+      .then((dataa) => {
+        const html = dataa.data;
+        switch (cur_shop) {
+          case 'www.wconcept.co.kr':
+            new_product = w_concept(html, url);
+            break;
+          case 'store.musinsa.com':
+            new_product = musinsa(html, url);
+            break;
+          case 'www.brandi.co.kr':
+            new_product = brandi(html, url);
+            break;
+          default:
+            break;
+        }
+      })
+      .catch(
+        // 리퀘스트 실패 - then 보다 catch 가 먼저 실행됨..
+        console.log('get shopping mall html request is failed')
+      );
+  }
+  // console.log(`new_product : ${JSON.stringify(new_product)}`)
+  return new_product;
+}
+
 
 module.exports = basketRouter;
