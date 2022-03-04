73e7b3c6ffc6c669c9effb07e422eade82013e86
JongWoo Han <jongwoo0221@gmail.com>
2022-03-04 17:44:21 +0900

    style: Change vote button in orivateBasket

--- client/src/components/views/PrivateBasket/PrivateBasket.jsx
+++ client/src/components/views/PrivateBasket/PrivateBasket.jsx
@@ -2,66 +2,70 @@ import React, { useState, useEffect } from 'react';
 import styles from './PrivateBasket.module.css';
 import Cookies from 'universal-cookie';
 import axios from 'axios';
+import Header from '../../header/Header';
 
 const PrivateBasket = (props) => {
-  const [token, setToken] = useState();
-  const [products, setProducts] = useState();
+  const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(false);
 
   const getCookie = (name) => {
     const cookies = new Cookies();
     return cookies.get(name);
   };
+  const token = getCookie('x_auth');
 
   useEffect(() => {
-    setToken(getCookie('x_auth'));
     axios
       .get(`/privatebasket/${token}`)
       .then((Response) => {
         console.log('token : ', token);
-        console.log(Response.data);
+        console.log('response data : ', Response.data);
         setProducts(Response.data.reverse());
       })
       .catch((Error) => {
         console.log(Error);
       })
-      .then(() => {
-        console.log('products : ', products);
-      });
+      .then(() => {});
   }, []);
+
+  console.log('products: ', products);
 
   return (
     <div>
+      <Header />
       <div className={styles.container}>
         <div className={styles.experienceContents}>
           <ul className={styles.experienceCategory}>
             <li className={styles.categoryName}>
-              <div className={styles.maskTest}>
+              <div className={styles.maskText}>
                 <span>장바구니</span>
               </div>
             </li>
             <li className={styles.categoryName}>
-              <div className={styles.maskTest}>
+              <div className={styles.maskText}>
                 <span>투표</span>
               </div>
             </li>
           </ul>
           <div className={styles.experienceGrid}>
             <div className={styles.experienceList}>
-              <div className={styles.experienceItem}>
-                <a className={styles.itemLink}>
-                  <div className={styles.itemFrame}>
-                    <div className={styles.itemDimd}></div>
-                    <div className={styles.itemVisual}>
-                      <div className={styles.imageBox}>
-                        <img />
-                      </div>
-                      <div className={styles.hoverBox}>
-                        <img />
+              {products.map((item, index) => (
+                <div className={styles.experienceItem}>
+                  <a className={styles.itemLink}>
+                    <div className={styles.itemFrame}>
+                      <div className={styles.itemDimd}></div>
+                      <div className={styles.itemVisual}>
+                        <div className={styles.imageBox}>
+                          <img />
+                        </div>
+                        <div className={styles.hoverBox}>
+                          <img className={styles.itemImg} src={item.removedBgImg} />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </a>
-              </div>
+                  </a>
+                </div>
+              ))}
             </div>
           </div>
           <div className={styles.experienceLoading}></div>
