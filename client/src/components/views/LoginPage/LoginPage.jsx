09a7767b671ff6279e5a213f3c91df1552eeafe3
huckjoo <huckjoo96@naver.com>
2022-03-09 18:22:15 +0900

    feat: change login localhost to moba

--- client/src/components/views/LoginPage/LoginPage.jsx
+++ client/src/components/views/LoginPage/LoginPage.jsx
@@ -58,7 +58,7 @@ function LoginPage(props) {
               <a
                 className={styles.kakao}
                 href={
-                  'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=497af053ca6574eb9e8a19b5797cf024&redirect_uri=http://localhost:8000/oauth/kakao'
+                  'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=497af053ca6574eb9e8a19b5797cf024&redirect_uri=https://moba-shop.net/oauth/kakao'
                 }
               >
                 <i className='fa-solid fa-comment fa-lg'></i>
